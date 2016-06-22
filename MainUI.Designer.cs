32bc430ac3b2d673108e276bf79cbe2f9b3220b5
Maikel van den Hout <mjwghout@avans.nl>
2016-06-22 19:41:10 +0200

    add tomsons ui tweaks

--- MainUI.Designer.cs
+++ MainUI.Designer.cs
@@ -34,24 +34,40 @@
             // textBoxOutput
             // 
             this.textBoxOutput.AcceptsReturn = true;
-            this.textBoxOutput.Font = new System.Drawing.Font("Lucida Console", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBoxOutput.Location = new System.Drawing.Point(34, 41);
+            this.textBoxOutput.AccessibleRole = System.Windows.Forms.AccessibleRole.StaticText;
+            this.textBoxOutput.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+            | System.Windows.Forms.AnchorStyles.Left)
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxOutput.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(15)))), ((int)(((byte)(15)))), ((int)(((byte)(15)))));
+            this.textBoxOutput.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBoxOutput.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.textBoxOutput.Font = new System.Drawing.Font("Lucida Console", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxOutput.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.textBoxOutput.Location = new System.Drawing.Point(8, 9);
+            this.textBoxOutput.Margin = new System.Windows.Forms.Padding(5);
             this.textBoxOutput.Multiline = true;
             this.textBoxOutput.Name = "textBoxOutput";
             this.textBoxOutput.ReadOnly = true;
-            this.textBoxOutput.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
-            this.textBoxOutput.Size = new System.Drawing.Size(777, 426);
+            this.textBoxOutput.ShortcutsEnabled = false;
+            this.textBoxOutput.Size = new System.Drawing.Size(551, 307);
             this.textBoxOutput.TabIndex = 0;
+            this.textBoxOutput.TabStop = false;
+            this.textBoxOutput.WordWrap = false;
             this.textBoxOutput.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // MainUI
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(878, 524);
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(15)))), ((int)(((byte)(15)))), ((int)(((byte)(15)))));
+            this.ClientSize = new System.Drawing.Size(569, 327);
             this.Controls.Add(this.textBoxOutput);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
             this.Name = "MainUI";
-            this.Text = "Form1";
+            this.ShowIcon = false;
+            this.Text = "Monochrome Memory Output";
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -62,4 +78,3 @@
         private System.Windows.Forms.TextBox textBoxOutput;
     }
 }
-
